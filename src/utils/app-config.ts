cb67fddf0140c19ed33485ac150f4551b65f4d81
luli-tech <olabodemicheal5@gmail.com>
2025-10-17 22:20:08 +0100

    logger

--- src/utils/app-config.ts
+++ src/utils/app-config.ts
@@ -1,7 +1,7 @@
 // src/utils/app-config.ts
 import { ConfigService } from '@nestjs/config';
 import { AppExceptions } from './exceptions';
-
+import { Logger } from '@nestjs/common';
 export class AppConfig {
   static PORT: number;
   static RATE_LIMIT: number;
@@ -9,6 +9,8 @@ export class AppConfig {
   static CAT_API_URL: string;
 
   static init(configService: ConfigService) {
+    const logger = new Logger('AppConfig');
+
     const port = Number(configService.get('PORT'));
     const rateLimitMax = Number(configService.get('RATE_LIMIT'));
     const rateTtl = Number(configService.get('RATE_TTL'));
@@ -23,5 +25,11 @@ export class AppConfig {
     AppConfig.RATE_LIMIT = rateLimitMax!;
     AppConfig.RATE_TTL = rateTtl!;
     AppConfig.CAT_API_URL = catApiUrl!;
+
+    logger.log('✅ Environment variables successfully loaded:');
+    logger.log(`PORT: ${AppConfig.PORT}`);
+    logger.log(`RATE_LIMIT: ${AppConfig.RATE_LIMIT}`);
+    logger.log(`RATE_TTL: ${AppConfig.RATE_TTL}`);
+    logger.log(`CAT_API_URL: ${AppConfig.CAT_API_URL}`);
   }
 }
