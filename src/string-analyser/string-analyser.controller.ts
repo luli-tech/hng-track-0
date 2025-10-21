2879e48986c2be0bdbcc2b399cc673aeac5f7ca4
luli-tech <olabodemicheal5@gmail.com>
2025-10-21 06:38:40 +0100

    statge 2 hng internship

--- src/string-analyser/string-analyser.controller.ts
+++ src/string-analyser/string-analyser.controller.ts
@@ -16,35 +16,34 @@ import { StringAnalyserService } from './string-analyser.service';
 export class StringAnalyserController {
   constructor(private readonly service: StringAnalyserService) {}
 
-  // 1️⃣ Create / Analyze String
+  //  Create / Analyze String
   @Post()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() dto: CreateStringAnalyserDto) {
     return this.service.createString(dto);
   }
 
-  // 2️⃣ Get All Strings (with Filtering)
+  //  Get All Strings (with Filtering)
   @Get()
   findAll(@Query() query: any) {
     return this.service.getAllStrings(query);
   }
 
-  // 3️⃣ Get Specific String
+  @Get('filter-by-natural-language')
+  filterByNaturalLanguage(@Query('query') query: string) {
+    return this.service.filterByNaturalLanguage(query);
+  }
+
+  //  Get Specific String
   @Get(':value')
   findOne(@Param('value') value: string) {
     return this.service.getString(value);
   }
 
-  // 4️⃣ Delete String
+  //  Delete String
   @Delete(':value')
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('value') value: string) {
     return this.service.deleteString(value);
-  }
-
-  // 5️⃣ Natural Language Filtering
-  @Get('filter-by-natural-language')
-  filterByNaturalLanguage(@Query('query') query: string) {
-    return this.service.filterByNaturalLanguage(query);
   }
 }
